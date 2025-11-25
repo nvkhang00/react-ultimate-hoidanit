07197514d8e114bd7a79406d636cddd2cf49b779
nvkhang00 <nvkhang0099@gmail.com>
2025-11-25 19:34:41 +0700

    video 74

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,4 @@
-import { Input, Button, notification } from 'antd';
+import { Input, Button, Modal } from 'antd';
 import { useState } from 'react';
 import { createUserAPI } from '../../services/api.service';
 import toast from 'react-hot-toast';
@@ -7,6 +7,20 @@ const UserForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [phone, setPhone] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const formatErrorMessage = (message) => {
+        if (Array.isArray(message)) {
+            return (
+                <ul style={{ backgroundColor: 'white', listStyle: 'inherit', paddingLeft: '20px', marginTop: '-5px' }}>
+                    {message.map((item, index) => (
+                        <li key={`${item}-${index}`}>{item}</li>
+                    ))}
+                </ul>
+            );
+        }
+        return message;
+    };
+
     const handleClick = async () => {
         const response = await createUserAPI(fullName, email, password, phone);
         if (response.data) {
@@ -14,55 +28,75 @@ const UserForm = () => {
                 duration: 2000,
                 position: 'top-right',
                 style: {
-                    minWidth: '250px'
+                    minWidth: '250px',
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    justifyContent: 'space-between'
                 }
             });
+            setIsModalOpen(false);
         } else {
-            toast.error(JSON.stringify(response.message), {
+            toast.error(formatErrorMessage(response.message), {
                 duration: 2000,
                 position: 'top-right',
                 style: {
-                    maxWidth: '250px'
+                    maxWidth: '320px',
+                    display: 'flex',
+                    alignItems: 'flex-start',
+                    justifyContent: 'space-between',
                 }
             });
         }
     }
     return (
-        <div className='user-form' style={{ margin: '20px 0' }}>
-            <div style={{ display: 'flex', gap: '15px', flexDirection: 'column' }}>
-                <div>
-                    <span>Full Name</span>
-                    <Input
-                        value={fullName}
-                        onChange={(e) => setFullname(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <span>Phone number</span>
-                    <Input
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <Button
-                        onClick={handleClick}
-                        type='primary' >Create User</Button>
-                </div>
+        <div className='user-form' style={{ margin: '10px 0' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h3>Table User</h3>
+                <Button
+                    onClick={() => setIsModalOpen(true)}
+                    type='primary' >
+                    Create User
+                </Button>
+                <Modal
+                    title="Create User"
+                    closable={{ 'aria-label': 'Custom Close Button' }}
+                    open={isModalOpen}
+                    onOk={handleClick}
+                    onCancel={() => setIsModalOpen(false)}
+                    okText={'Create'}
+                    maskClosable={false}
+                >
+                    <div style={{ display: 'flex', gap: '15px', flexDirection: 'column' }}>
+                        <div>
+                            <span>Full Name</span>
+                            <Input
+                                value={fullName}
+                                onChange={(e) => setFullname(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <span>Email</span>
+                            <Input
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <span>Password</span>
+                            <Input.Password
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <span>Phone number</span>
+                            <Input
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </Modal>
             </div>
         </div>
     );
