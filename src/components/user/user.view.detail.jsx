2ef46471d7a96437fc687373cca685bee19416c9
nvkhang00 <nvkhang0099@gmail.com>
2025-11-28 08:30:18 +0700

    video 82

--- src/components/user/user.view.detail.jsx
+++ src/components/user/user.view.detail.jsx
@@ -7,6 +7,7 @@ const UserDetail = (
 ) => {
     return (
         <Drawer
+            width={'40vw'}
             title="User Detail"
             closable={{ 'aria-label': 'Close Button' }}
             onClose={onClose}
@@ -19,6 +20,36 @@ const UserDetail = (
                         <p>Full name: {detailUserData.fullName}</p>
                         <p>Email: {detailUserData.email}</p>
                         <p>Phone number: {detailUserData.phone}</p>
+                        <p>Avatar:</p>
+                        <div style={{ width: '300px', height: '250px' }}>
+                            <img
+                                src={`${import.meta.env.VITE_BASE_ULR}/images/avatar/${detailUserData.avatar}`}
+                                alt="avatar"
+                                style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    objectFit: 'cover',
+                                }}
+                            />
+                        </div>
+                        <div>
+                            <label
+                                style={{
+                                    display: 'block',
+                                    width: 'fit-content',
+                                    height: '30px',
+                                    backgroundColor: 'orange',
+                                    padding: '8px 15px',
+                                    marginTop: '10px',
+                                    borderRadius: '8px',
+                                    cursor: 'pointer',
+                                }}
+                                htmlFor='btnUpload'
+                            >
+                                Upload Avatar
+                            </label>
+                            <input type="file" id='btnUpload' hidden />
+                        </div>
                     </>
                     :
                     <p>No Data</p>
